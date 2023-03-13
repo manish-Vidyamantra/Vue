87384fc112d1fdc27e7c5368e43ea6ad8cde6fa4
manish-Vidyamantra <manish.prasad@vidyamantra.com>
2023-03-13 16:32:21 +0530

    test done

---
+++ vueTest2Form/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import InputBox from './components/InputBox.vue'
+
+const validate = ref(null);
+
+function handleClick(e) {
+  e.preventDefault();
+  console.log('run')
+  validate.value.validateForm();
+}
+
+</script>
+
+<template>
+  <div class="w-1/2 h-2/3 flex justify-center items-center bg-gray-800 rounded-md">
+    <form class=" flex flex-col justify-center items-center w-full h-full">
+      <h1 class="text-5xl font-medium text-white">Registration Form</h1>
+        <InputBox ref="validate" />
+        <button @click="handleClick" class="bg-white p-4 rounded-lg mb-5" type="submit">Create Account</button>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vueTest2Form/src/components/InputBox.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+  import {ref, defineExpose} from 'vue';
+
+  defineExpose({validateForm});
+  
+  let userNameRef = ref(null);
+  let emailRef = ref(null);
+  let passwordRef = ref(null);
+  
+  let isUserNameNotValid = ref(false);
+  let isEmailNotValid = ref(false);
+  let isPasswordNotValid = ref(false);
+
+  function validateForm() {
+    console.log('called')
+    let userName = userNameRef._value.value.trim();
+    let email = emailRef._value.value.trim();
+    let password = passwordRef._value.value.trim();
+
+    if(!userName) {
+      isUserNameNotValid.value = true;
+    } else {
+      isUserNameNotValid.value = false;
+    }
+    if(!email) {
+      isEmailNotValid.value = true; 
+    } else {
+      isEmailNotValid.value = false; 
+    }
+    if(!password) {
+      isPasswordNotValid.value = true;
+    } else {
+      isPasswordNotValid.value = false;
+    }
+
+    if((!userName || (!email) || (!password))) {
+      alert('Form Not submitted')
+    } else {
+      alert('Form submitted successfully');
+    }
+
+  }
+</script>
+
+<template>
+
+  <div class="flex flex-col m-5 w-96">
+    <input ref ="userNameRef" type="text" placeholder="Full name" class=" m-4 p-5 border-2 border-black rounded-md">
+    <p v-if="isUserNameNotValid" class="text-red-300 text-center">Full name is incorrect</p>
+    <input ref ="emailRef" type="email" placeholder="Email" class=" m-4 p-5 rounded-md border-2 border-black">
+    <p v-if="isEmailNotValid" class="text-red-300 text-center">Email is incorrect</p>
+    <input ref ="passwordRef" type="password" placeholder="Password" class="m-4 p-5 rounded-md border-2 border-black">
+    <p v-if="isPasswordNotValid" class="text-red-300 text-center">password is incorrect</p>
+  </div>
+  
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
